9cabfb00bd395d5e8582ff7678fbcb5446b06df5
simal24k <simal.mpgss@gmail.com>
2025-05-02 22:38:41 +0500

    Update Q3.cpp

--- LAB 10/Q3.cpp
+++ LAB 10/Q3.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+int main() {
+    ifstream ch1("chapter1.txt"), ch2("chapter2.txt");
+    ofstream book("book.txt", ios::app);
+
+    string line;
+    while (getline(ch1, line)) book << line << endl;
+    book << endl;
+    while (getline(ch2, line)) book << line << endl;
+
+    ch1.close();
+    ch2.close();
+    book.close();
+
+    ifstream showBook("book.txt");
+    cout << "\nCombined Book:\n";
+    while (getline(showBook, line)) {
+        cout << line << endl;
+    }
+    showBook.close();
+    return 0;
+}
