7bd4fc67cd7aa1832bf81c551de1f97027c8b7d8
simal24k <simal.mpgss@gmail.com>
2025-05-02 22:42:08 +0500

    Update Q5.cpp

--- LAB 10/Q5.cpp
+++ LAB 10/Q5.cpp
@@ -1,24 +1,4 @@
-// Q5. You're building a tool to search and replace a specific word in a text file without
-// rewriting the entire file.
-// Requirements:
-
-// 1. Create and populate a file named data.txt with a few sentences. (e.g., AI is the
-// future. AI will transform the world. Embrace AI now.)
-// 2. Prompt the user to enter:
-// ○ A searchWord (e.g., "AI")
-// ○ A replacementWord (e.g., "ML")
-// 3. Open the file using fstream in ios::in | ios::out mode.
-// 4. Search for the searchWord using seekg() and read sequentially.
-// 5. When found:
-// ○ Use tellg() to get the current read pointer location.
-// ○ Use seekp() to position the write pointer.
-// ○ Replace the word only if the replacement is of equal or smaller length to
-// avoid shifting content.
-
-// 6. If the word is replaced, pad with spaces if needed (e.g., replacing "AI" with "ML" or
-// "XX").
-// 7. After all replacements, display the updated file.
-#include <iostream>
+include <iostream>
 #include <fstream>
 using namespace std;
 
@@ -83,3 +63,4 @@ int main() {
 
     return 0;
 }
+//sself note:Tests in-place word replacement using file pointers and padding logic.
