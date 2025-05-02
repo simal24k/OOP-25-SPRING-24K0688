60f3a3d8f461f18d007e95c55a58433c7ee6e90b
simal24k <simal.mpgss@gmail.com>
2025-05-02 22:39:09 +0500

    Update Q4.cpp

--- LAB 10/Q4.cpp
+++ LAB 10/Q4.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+int main() {
+    string text = "C++ is a powerful programming language.";
+    ofstream fileOut("info.txt");
+    fileOut << text;
+    fileOut.close();
+
+    fstream file("info.txt", ios::in | ios::out);
+    cout << "Initial get pointer: " << file.tellg() << endl;
+    cout << "Initial put pointer: " << file.tellp() << endl;
+
+    file.seekg(6);
+    string word;
+    file >> word;
+    cout << "Read word at position 6: " << word << endl;
+
+    file.seekp(6);
+    file << "dynamic"; // overwrite "powerful" (8 characters)
+
+    cout << "New get pointer: " << file.tellg() << endl;
+    cout << "New put pointer: " << file.tellp() << endl;
+    file.close();
+
+    ifstream readFile("info.txt");
+    string updated;
+    getline(readFile, updated);
+    cout << "\nUpdated File Content:\n" << updated << endl;
+    readFile.close();
+    return 0;
+}
